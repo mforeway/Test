68a81fb9b2084a5a50b8b74b904b35291da4c30c
Michael <michael.kutsenko@foreway.pro>
2017-06-16 10:41:12 +0300

    two

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("master");
+
+        System.out.println("two");
+
+        System.out.println("new string");
     }
 }
