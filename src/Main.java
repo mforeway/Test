11517ac7e73af30ed85f80f686ecbad3a327e633
Michael <michael.kutsenko@foreway.pro>
2017-06-16 10:39:25 +0300

    delete all

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Buy");
-
-        System.out.println("hi");
-
-        System.out.println("new message");
-
-        System.out.println("the newest");
-
-
-        System.out.println("it will be another");
+        System.out.println("master");
     }
 }
